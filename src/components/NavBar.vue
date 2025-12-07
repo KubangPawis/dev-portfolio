fc801127a09ceecfe38c27882e5f8d74ebf56313
KubangPawis <xanderfernandezperegrin@gmail.com>
2025-12-07 15:33:07 +0800

    feat: add app-wide NavBar; implement page content centering

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="inline-flex items-center text-base font-medium px-6 py-2 gap-x-6 border border-(--navbar-accent) rounded-4xl"
+  >
+    <p>Home</p>
+    <p>Work</p>
+    <p>Projects</p>
+    <p>Achievements</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="flex flex-wrap justify-between items-center">
-    <h1 class="text-[56px]">Hi</h1>
+    <h1 class="text-[56px] font-bold">Hey!</h1>
+    <NavBar />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue'
+</script>
 
 <style scoped></style>
