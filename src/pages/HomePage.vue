e1f5c3b48b0e10cd86cbe32fdfaae06d1e16310c
KubangPawis <xanderfernandezperegrin@gmail.com>
2025-12-07 15:10:58 +0800

    feat: add home page
    - Added base Home page design
    
    - Implemented page routing

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-wrap justify-between items-center">
+    <h1 class="text-[56px]">Hi</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
